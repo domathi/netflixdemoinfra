13068fa2b2d1af7906b52c91ffb8ca951e9fe1c4
domathi <domathipujari1919@gmail.com>
2025-08-13 17:25:34 +0530

    Update main.tff

--- main.tf
+++ main.tf
@@ -1,18 +1,20 @@
 provider "aws" {
-region = "ap-south-1"
-}
-
-resource "aws_instance" "one" {
-count = 5
-ami = "ami-0120bca902526ae75"
-instance_type = "t2.medium"
-key_name = "kskp"
-vpc_security_group_ids = ["sg-0fceeb40d5eee0ade"]
-tags = {
-Name = var.instance_names[count.index]
-}
+  region = "ap-south-1"
 }
 
 variable "instance_names" {
-default = ["jenkins", "nexus", "AppServer-1", "AppServer-2", "Monitoring server"]
+  default = ["jenkins", "nexus", "AppServer-1", "AppServer-2", "Monitoring server"]
 }
+
+resource "aws_instance" "one" {
+  count                  = length(var.instance_names) # Automatically match number of names
+  ami                    = "ami-0120bca902526ae75"
+  instance_type          = "t2.medium"
+  key_name               = "kskp"
+  vpc_security_group_ids = ["sg-0fceeb40d5eee0ade"]
+
+  tags = {
+    Name = var.instance_names[count.index]
+  }
+}
+
