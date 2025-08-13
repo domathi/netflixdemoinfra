311b03bd915f37dc71e20a657ebcd10f1a97c0dc
domathi <domathipujari1919@gmail.com>
2025-08-13 16:50:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 5
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
-key_name = "swikp"
+key_name = "saikp"
 vpc_security_group_ids = ["sg-02d39bf1d2150a0c6"]
 tags = {
 Name = var.instance_names[count.index]
