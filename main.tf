a1face2ab987598cda9e8c5506fa069101786e94
domathi <domathipujari1919@gmail.com>
2025-08-13 16:23:59 +0530

    Update main.tff
    changed sg group

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 5
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
 key_name = "swikp"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+vpc_security_group_ids = ["sg-02d39bf1d2150a0c6"]
 tags = {
 Name = var.instance_names[count.index]
 }
